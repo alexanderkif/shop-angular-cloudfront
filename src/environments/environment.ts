ec138bbbba40329474a58995aa82e64cef04668e
Aleksandr Nikiforov <aleksandr_nikiforov@epam.com>
2023-03-05 13:02:37 +0300

    bff switched to true

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,16 +8,16 @@ export const environment: Config = {
   production: false,
   apiEndpoints: {
     product: 'https://sgvz5ygvqk.execute-api.eu-west-1.amazonaws.com/products',
-    order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    order: 'https://sgvz5ygvqk.execute-api.eu-west-1.amazonaws.com',
+    import: 'https://sgvz5ygvqk.execute-api.eu-west-1.amazonaws.com',
+    bff: 'https://sgvz5ygvqk.execute-api.eu-west-1.amazonaws.com',
+    cart: 'https://sgvz5ygvqk.execute-api.eu-west-1.amazonaws.com',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
     import: false,
-    bff: false,
+    bff: true,
     cart: false,
   },
 };
